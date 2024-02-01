0b3d95452873e51d15d4aff8345e1ed8ebbd9fdc
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2024-02-01 15:41:49 +0800

    feat: v1

--- public/pdfWorker.worker.js
+++ public/pdfWorker.worker.js
@@ -1,117 +1,20 @@
-// // pdfWorker.js;
-// importScripts(
-//   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-// );
-
-// self.onmessage = async function (event) {
-//   const { imgData, filename } = event.data;
-//   const pdf = new self.jspdf.jsPDF('p', 'mm', 'a4');
-//   console.log('imgData', imgData);
-//   pdf.addImage(
-//     imgData,
-//     'PNG',
-//     0,
-//     0,
-//     pdf.internal.pageSize.getWidth(),
-//     pdf.internal.pageSize.getHeight(),
-//   );
-
-//   // Save the PDF
-//   await pdf.save(filename);
-
-//   self.postMessage('PDF generated successfully!');
-// };
-// importScripts(
-//   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.1.1/jspdf.umd.min.js',
-// );
-// importScripts(
-//   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js',
-// );
-
-// self.onmessage = async function (event) {
-//   console.log('Worker received message:', event.data);
-
-//   const { imgData, filename } = event.data;
-
-//   try {
-//     const pdf = new self.jspdf.jsPDF('p', 'mm', 'a4');
-//     console.log('1');
-//     pdf.addImage(
-//       imgData,
-//       'PNG',
-//       0,
-//       0,
-//       pdf.internal.pageSize.getWidth(),
-//       pdf.internal.pageSize.getHeight(),
-//     );
-//     console.log('2', pdf);
-//     const output = pdf.output();
-//     console.log('PDF output:', output);
-
-//     pdf.save(filename);
-//     self.postMessage('PDF generated successfully!');
-//   } catch (error) {
-//     console.error('Error generating PDF:', error);
-//     self.postMessage('PDF generation failed.');
-//   }
-// };
 importScripts(
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.1.1/jspdf.umd.min.js',
-);
-importScripts(
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js',
 );
 
 self.onmessage = async function (event) {
   console.log('Worker received message:', event.data);
 
-  const { imgData, filename } = event.data;
-  // 1 canvas 宽高
-  // const contentWidth = canvas.width;
-  // const contentHeight = canvas.height;
-  // // 2 一页 pdf 显示 html 页面生成的 canvas 高度
-  // const pageHeight = (contentWidth / 592.28) * 841.89;
-  // // 3 未生成 pdf 的 html 页面高度
-  // let leftHeight = contentHeight;
-  // // 4 pdf 页面偏移
-  // let position = 0;
-  // // 5 a4纸的尺寸 [595.28, 841.89]，html 页面生成的 canvas 在 pdf 中图片的宽高
-  // const imgWidth = 595.28;
-  // const imgHeight = (592.28 / contentWidth) * contentHeight;
-
+  const { imgDataArray, filename } = event.data;
   try {
     const pdf = new self.jspdf.jsPDF('p', 'mm', 'a4');
 
-    // // 当内容未超过 pdf 一页显示的范围，无需分页
-    // if (leftHeight < pageHeight) {
-    //   pdf.addImage(img, 'JPEG', 0, 0, imgWidth, imgHeight);
-    // } else {
-    //   while (leftHeight > 0) {
-    //     console.log(imgWidth, imgHeight, position, leftHeight);
-    //     pdf.addImage(img, 'JPEG', 0, position, imgWidth, imgHeight);
-    //     leftHeight -= pageHeight;
-    //     position -= 841.89;
-    //     // 避免添加空白页
-    //     if (leftHeight > 0) {
-    //       pdf.addPage();
-    //     }
-    //   }
-    // }
-    // // 6 挂载至页面
-    // const blob = dataURLtoBlob(pdf.output('datauristring'));
-    // console.log(blob);
-    // const url = window.URL.createObjectURL(blob); //获得一个pdf的url对象
-    // location.href = url;
-    pdf.addImage(
-      imgData,
-      'PNG',
-      0,
-      0,
-      pdf.internal.pageSize.getWidth(),
-      pdf.internal.pageSize.getHeight(),
-    );
-
-    const pdfData = pdf.output('arraybuffer'); // Get the PDF data as ArrayBuffer
+    imgDataArray.forEach(({ dataUrl, width, height }) => {
+      pdf.addPage();
+      pdf.addImage(dataUrl, 'PNG', 0, 0, 595.28, (595.28 / width) * height);
+    });
+    console.log('b');
+    const pdfData = pdf.output('arraybuffer');
     self.postMessage({ pdfData, filename });
   } catch (error) {
     console.error('Error generating PDF:', error);
--- src/pages/Table/index.tsx
+++ src/pages/Table/index.tsx
@@ -1,138 +1,115 @@
-// // Home.jsx
-// import { Button } from 'antd';
-// import html2canvas from 'html2canvas';
-// import { useRef } from 'react';
-// import ProTable from './components/ProTable';
-
-// const Home = () => {
-//   const contentRef = useRef(null);
-
-//   const handleClick = () => {
-//     const htmlContent = contentRef.current;
-//     if (!htmlContent) {
-//       console.error('HTML content is null or undefined');
-//       return;
-//     }
-//     html2canvas(htmlContent, { scale: 2 }).then((canvas) => {
-//       const imgData = canvas.toDataURL('image/png');
-//       const worker = new Worker('/public/pdfWorker.worker.js');
-
-//       worker.onmessage = (event) => {
-//         console.log(event.data);
-//         worker.terminate();
-//       };
-
-//       worker.onerror = (error) => {
-//         console.error(error);
-//         worker.terminate();
-//       };
-
-//       const filename = 'output.pdf';
-//       worker.postMessage({ imgData, filename });
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <Button onClick={handleClick}>导出</Button>
-//       <div ref={contentRef}>
-//         <div>aaa</div>
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//         <ProTable />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
 import { Button } from 'antd';
 import { saveAs } from 'file-saver';
 import html2canvas from 'html2canvas';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import ProTable from './components/ProTable';
 const Home = () => {
   const contentRef = useRef(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
+  const getSections = (content: any) => {
+    const sections = [];
+    const pageSize = 800; // You can adjust this value based on your requirements
+
+    let offset = 0;
+    while (offset < content.clientHeight) {
+      const section = document.createElement('div');
+      section.style.height = `${pageSize}px`;
+      section.style.overflow = 'hidden';
+
+      const clonedContent = content.cloneNode(true);
+      clonedContent.style.transform = `translate(0, -${offset}px)`;
+      section.appendChild(clonedContent);
+
+      sections.push(section);
+      offset += pageSize;
+    }
+
+    return sections;
+  };
+  async function captureSection(section: any) {
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'none';
+    wrapper.appendChild(section.cloneNode(true));
+    document.body.appendChild(wrapper);
+
+    const canvas = await html2canvas(wrapper, { scale: 1 });
+    const contentWidth = canvas.width;
+    const contentHeight = canvas.height;
+    const imgWidth = 595.28;
+    const imgHeight = (592.28 / contentWidth) * contentHeight;
+    return {
+      dataUrl: canvas.toDataURL('image/png'),
+      width: imgWidth,
+      height: imgHeight,
+    };
+  }
+
+  const handleClick = async () => {
+    setLoading(true);
     const htmlContent = contentRef.current;
     if (!htmlContent) {
       console.error('HTML content is null or undefined');
       return;
     }
-    const devicePixelRatio = window.devicePixelRatio || 1;
-    const scale = 1 / devicePixelRatio;
 
-    // Calculate the canvas dimensions based on the scaled size
-    // @ts-ignore
-    const canvasWidth = htmlContent.offsetWidth * devicePixelRatio;
-    // @ts-ignore
-    const canvasHeight = htmlContent.offsetHeight * devicePixelRatio;
-    html2canvas(htmlContent, {
-      useCORS: true,
-      allowTaint: true,
-      scale,
-      width: canvasWidth,
-      height: canvasHeight,
-    }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
+    const sections = getSections(htmlContent);
+    console.log('sections', sections);
 
-      const worker = new Worker('/public/pdfWorker.worker.js');
+    const imgDataArray = [];
 
-      worker.onmessage = (event) => {
-        const { pdfData, filename, error } = event.data;
-        if (pdfData) {
-          const pdfBlob = new Blob([pdfData], { type: 'application/pdf' });
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const { dataUrl, width, height } = await captureSection(section);
+      imgDataArray.push({ dataUrl, width, height });
+    }
+    const worker = new Worker('/public/pdfWorker.worker.js');
+    worker.onmessage = (event) => {
+      const { pdfData, filename, error } = event.data;
+      if (pdfData) {
+        const pdfBlob = new Blob([pdfData], { type: 'application/pdf' });
 
-          // Use FileSaver.js to save the PDF
-          saveAs(pdfBlob, filename);
+        saveAs(pdfBlob, filename);
+        setLoading(false);
+        console.log('PDF saved successfully!');
+      } else {
+        console.error('PDF generation failed:', error);
+      }
 
-          console.log('PDF saved successfully!');
-        } else {
-          console.error('PDF generation failed:', error);
-        }
+      worker.terminate();
+    };
 
-        worker.terminate();
-      };
+    worker.onerror = (error) => {
+      console.error('Worker error:', error);
+      worker.terminate();
+    };
 
-      worker.onerror = (error) => {
-        console.error('Worker error:', error);
-        worker.terminate();
-      };
-
-      const filename = 'output.pdf';
-      worker.postMessage({ imgData, filename });
-    });
+    const filename = 'output.pdf';
+    worker.postMessage({ imgDataArray, filename });
   };
 
   return (
     <div>
-      <Button onClick={handleClick}>Export as PDF</Button>
+      <Button loading={loading} onClick={handleClick}>
+        Export as PDF
+      </Button>
       <div ref={contentRef}>
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
-        <ProTable />
+        <img
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          alt=""
+        />
+        <img
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          alt=""
+        />
+        <img
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          alt=""
+        />
+        <img
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          alt=""
+        />
         <ProTable />
         <ProTable />
         <ProTable />
